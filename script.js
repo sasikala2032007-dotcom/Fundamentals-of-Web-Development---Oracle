443d322ae9023becf8f6b773904a25d85da4c86a
sasikala2032007-dotcom <sasikala2032007@gmail.com>
2026-09-25 20:47:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+</head>
+<body>
+
+    <h1 id="heading">Hello World</h1>
+    <button onclick="changeText()">Click Me</button>
+
+    <script src="script.js"></script>
+</body>
+</html>
