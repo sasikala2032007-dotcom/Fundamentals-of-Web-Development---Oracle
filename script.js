fc1a9d45f1c2dce51c9f2acd2267f9cc34b5e7fa
sasikala2032007-dotcom <sasikala2032007@gmail.com>
2026-09-26 11:25:56 +0530

    Refactor changeText function to update heading text

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-```javascript
-// Get heading element
-const heading = document.getElementById("heading");
-
-// Button click function
 function changeText() {
-    heading.innerHTML = "Welcome to My Website!";
+    document.getElementById("heading").innerHTML = "Hello! Welcome to My Project";
 }
-
-// Change background color
-function changeBackground() {
-    document.body.style.backgroundColor = "lightblue";
-}
-
-// Show alert message
-function showMessage() {
-    alert("Hello! Welcome to my website.");
-}
-```
